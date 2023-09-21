33c5a60f5ab1a83b3eef7cc6ea12bc8a68f3cc88
RamakrishnaGTestAutomation <ramakrishna@winitsoftware.com>
2023-09-21 11:39:40 +0530

    parameterization added

--- src/test/java/TestWhatsApp.java
+++ src/test/java/TestWhatsApp.java
@@ -1,9 +1,8 @@
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.interactions.Actions;
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.Test;
-import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.*;
+
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.concurrent.TimeUnit;
@@ -14,11 +13,12 @@ import io.appium.java_client.windows.WindowsDriver;
 
 public class TestWhatsApp {
     public WindowsDriver driver = null;
-    @BeforeMethod
+    @BeforeTest
     public void setUp(){
         DesiredCapabilities cap = new DesiredCapabilities();
 //windows application id needs to be given to open the app. Run Get-StartApps in powershell to get all windows app ids.
         cap.setCapability("app","5319275A.WhatsAppDesktop_cv1g1gvanyjgm!App");
+
         //cap.setCapability("app", " Microsoft.WindowsNotepad_8wekyb3d8bbwe!App");
 //add platformname and device name
         cap.setCapability("platformName", "Windows");
@@ -34,28 +34,31 @@ public class TestWhatsApp {
 //provide implicit wait of 10 seconds
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
     }
-    @AfterMethod
-    public void teardown(){
+    @AfterTest
+    public void teardown()
+    {
+        driver.close();
         System.out.println("test run successful");
     }
-    @Test
-    public void sendmessagetogroups() throws InterruptedException {
 
-        driver.findElementByAccessibilityId("SearchQueryTextBox").sendKeys("Jkytest");
-
-        driver.findElement(By.name("Jkytest")).click();
-
+    @Test(dataProvider = "groups")
+    public void sendmessagetogroups(String groupname) throws InterruptedException {
+        driver.findElementByAccessibilityId("SearchQueryTextBox").sendKeys(groupname);
+        driver.findElement(By.name(groupname)).click();
         //driver.findElementByAccessibilityId("InputBarTextBox").sendKeys("msg");
         driver.findElementByAccessibilityId("InputBarTextBox").sendKeys(Keys.CONTROL, "v");
         driver.findElementByAccessibilityId("InputBarTextBox").sendKeys(Keys.ENTER);
         Thread.sleep(3000);
+    }
 
-       // driver.findElement(By.name("Jkytest2")).click();
-        //driver.findElementByAccessibilityId("InputBarTextBox").sendKeys(Keys.CLEAR);
-
-        //driver.findElementByAccessibilityId("InputBarTextBox").sendKeys("msg");
-
-       // driver.findElementByAccessibilityId("InputBarTextBox").sendKeys(Keys.ENTER);
+    @DataProvider(name = "groups")
+    public Object[] getGroupNames() {
+        Object[] data = {"Jkytest", "Jkytest2"};
+        return data;
+    }
 
     }
-}
+
+
+
+
